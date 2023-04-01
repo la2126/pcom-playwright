e4090e54352029b96ff9df5d4f64f4e9a63069df
mikeedwards83 <mike@glass.lu>
2023-04-01 11:44:45 +0100

    Adds plop storybook templates

---
+++ plopfile.js
@@ -0,0 +1,34 @@
+module.exports = function (plop) {
+    // controller generator
+    plop.setGenerator('story', {
+        description: 'Add Story',
+        prompts: [{
+            type: 'list',
+            name: 'type',
+            message: 'Component Type',
+            choices: ["atom", "component", "page", "material", "kontent"]
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Component Name'
+        }],
+        actions: [{
+            type: 'add',
+            path: 'stories/{{type}}s/{{name}}/{{name}}.tsx',
+            templateFile: 'plop-templates/Component.tsx.hbs'
+        },
+        {
+            type: 'add',
+            path: 'stories/{{type}}s/{{name}}/{{name}}.stories.tsx',
+            templateFile: 'plop-templates/Component.stories.tsx.hbs'
+        },
+        {
+            type: 'add',
+            path: 'stories/{{type}}s/{{name}}/{{name}}.scss',
+            templateFile: 'plop-templates/Component.scss.hbs'
+        }]
+    });
+
+ 
+};
